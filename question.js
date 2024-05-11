501dbe132a5d593ab4f5ea2a0162f20b2bbfb410
trinugroho2000i <167153373+trinugroho2000i@users.noreply.github.com>
2024-05-11 11:34:25 +0700

    Add files via upload

---
+++ question.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "4 x 7 = ... ",
+	"option1": "24",
+	"option2": "28",
+	"option3": "14",
+	"option4": "32",
+	"answer": "2"
+}, {
+	"question": "3 x 9 = ... ",
+	"option1": "21",
+	"option2": "12",
+	"option3": "27",
+	"option4": "18",
+	"answer": "3"
+}, {
+	"question": "5 x 6 = ...",
+	"option1": "30",
+	"option2": "35",
+	"option3": "40",
+	"option4": "45",
+	"answer": "1"
+}, {
+	"question": "6 x 7 = ... ",
+	"option1": "32",
+	"option2": "36",
+	"option3": "40",
+	"option4": "42",
+	"answer": "4"
+}, {
+	"question": "9 x 8 = ... ",
+	"option1": "17",
+	"option2": "64",
+	"option3": "72",
+	"option4": "81",
+	"answer": "3"
+}, {
+	"question": "3 x 2 x 6 = ...",
+	"option1": "24",
+	"option2": "36",
+	"option3": "42",
+	"option4": "48",
+	"answer": "2"
+}, {
+	"question": "2 x 4 x 7 = ...",
+	"option1": "40",
+	"option2": "42",
+	"option3": "56",
+	"option4": "60",
+	"answer": "3"
+}, {
+	"question": "2 x 5 x 8 = ... ",
+	"option1": "40",
+	"option2": "60",
+	"option3": "70",
+	"option4": "80",
+	"answer": "4"
+}, {
+	"question": "2 x 2 x 9 = ... ",
+	"option1": "36",
+	"option2": "40",
+	"option3": "45",
+	"option4": "46",
+	"answer": "1"
+}, {
+	"question": "5 x 2 x 7 = ... ",
+	"option1": "27",
+	"option2": "70",
+	"option3": "72",
+	"option4": "77",
+	"answer": "2"
+}];
---
+++ question_latihan.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "2 x 3 = ... ",
+	"option1": "6",
+	"option2": "8",
+	"option3": "14",
+	"option4": "32",
+	"answer": "1"
+}, {
+	"question": "3 x 4 = ... ",
+	"option1": "21",
+	"option2": "12",
+	"option3": "27",
+	"option4": "18",
+	"answer": "2"
+}, {
+	"question": "4 x 5 = ...",
+	"option1": "16",
+	"option2": "20",
+	"option3": "40",
+	"option4": "45",
+	"answer": "2"
+}, {
+	"question": "5 x 7 = ... ",
+	"option1": "35",
+	"option2": "36",
+	"option3": "40",
+	"option4": "42",
+	"answer": "1"
+}, {
+	"question": "6 x 8 = ... ",
+	"option1": "48",
+	"option2": "54",
+	"option3": "72",
+	"option4": "81",
+	"answer": "1"
+}, {
+	"question": "7 x 7 = ...",
+	"option1": "49",
+	"option2": "63",
+	"option3": "42",
+	"option4": "48",
+	"answer": "1"
+}, {
+	"question": "7 x 8 = ...",
+	"option1": "48",
+	"option2": "56",
+	"option3": "56",
+	"option4": "60",
+	"answer": "2"
+}, {
+	"question": "9 x 8 = ... ",
+	"option1": "72",
+	"option2": "88",
+	"option3": "70",
+	"option4": "80",
+	"answer": "1"
+}, {
+	"question": "9 x 6 = ... ",
+	"option1": "54",
+	"option2": "63",
+	"option3": "45",
+	"option4": "46",
+	"answer": "1"
+}, {
+	"question": "10 x 7 = ... ",
+	"option1": "75",
+	"option2": "70",
+	"option3": "72",
+	"option4": "77",
+	"answer": "2"
+}];
